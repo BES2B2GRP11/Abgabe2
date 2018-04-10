78ea01ae770b8d50cd68c9911971432b386facff
Ovidiu - Dan Bogat <ic17b501@technikum-wien.at>
2018-04-10 20:22:06 +0200

    Makefile bereinigt distclean, examples ordner erstellt fuer die einzelnen funktionen

---
+++ examples/fork.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fork.c
+ *
+ *    Description:  shows a fork 
+ *
+ *        Version:  1.0.0
+ *        Created:  04/10/2018 09:40:05 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ovidiu - Dan Bogat [ic17b501], ic17b501@technikum-wien.at
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "fork.h"
+
+int main(int argc, char** argv)
+{
+	argc=argc;
+	argv=argv;	
+  return (EXIT_SUCCESS);
+}
