70ad796b6baa5d8d8a8f20cd87f0569b0a05a52c
Ovidiu - Dan Bogat <ic17b501@technikum-wien.at>
2018-04-11 16:45:05 +0200

    Fork example

--- examples/fork.c
+++ examples/fork.c
@@ -16,11 +16,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
 #include "fork.h"
 
 int main(int argc, char** argv)
 {
 	argc=argc;
 	argv=argv;	
+	pid_t fork_pid;
+	
+	fork_pid = fork();
+	
+	if(fork_pid == 0)
+	{
+		//Child process
+		fprintf(stdout,"Child process with pid %d ppid %d\n", getpid(), getppid());
+	}else if(fork_pid < 0)
+	{
+		//Fork failed
+		fprintf(stderr,"[FATAL] fork() failed %s\n",strerror(errno));
+		exit(errno);
+	}else{
+		//parent process
+		fprintf(stdout,"Parent process with pid %d ppid %d\n", getpid(), getppid());
+	}
+	
   return (EXIT_SUCCESS);
 }
